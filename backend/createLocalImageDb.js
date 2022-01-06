5d9b575a99d9defc84d62213bbefa98a4a75f8d8
fwornle <fwornle@yahoo.co.uk>
2022-01-06 19:54:16 +0100

    update: local image DB creator script

--- backend/createLocalImageDb.js
+++ backend/createLocalImageDb.js
@@ -5,8 +5,8 @@ const path = require('path');
 const fs = require('fs');
 
 const url = [];
-// 100 fake avatar url from fakter
-for (let i = 0; i < 100; i++) {
+// 100 fake avatar url from faker
+for (let i = 0; i < 15; i++) {
   url.push(faker.image.people());
 }
 
@@ -14,7 +14,7 @@ for (let i = 0; i < 100; i++) {
 // download 100 user photoes to local image folder
 for (let i = 0; i < url.length; i++) {
   // path for image storage
-  const imagePath = path.join(__dirname, './images/people', `${i}.jpg`);
+  const imagePath = path.join(__dirname, './public/images/smobUsers', `${i}.jpg`);
   axios({
     method: 'get',
     url: url[i],
@@ -26,19 +26,40 @@ for (let i = 0; i < url.length; i++) {
 }
 
 const url2 = [];
-// 100 fake avatar url from fakter
-for (let i = 0; i < 100; i++) {
-  url2.push(faker.image.imageUrl(640, 480, 'animals', true));
+// 100 fake avatar url from faker
+for (let i = 0; i < 30; i++) {
+  url2.push(faker.image.imageUrl(640, 480, 'food', true));
 }
 
 
 // download 100 user photoes to local image folder
 for (let i = 0; i < url2.length; i++) {
   // path for image storage
-  const imagePath = path.join(__dirname, './images/products', `${i}.jpg`);
+  const imagePath = path.join(__dirname, './public/images/smobProducts', `${i}.jpg`);
   axios({
     method: 'get',
     url: url2[i],
+    responseType: 'stream',
+  })
+    .then((response) => {
+      response.data.pipe(fs.createWriteStream(imagePath));
+    });
+}
+
+const url3 = [];
+// 100 fake avatar url from faker
+for (let i = 0; i < 10; i++) {
+  url3.push(faker.image.imageUrl(640, 480, 'city', true));
+}
+
+
+// download 100 user photoes to local image folder
+for (let i = 0; i < url3.length; i++) {
+  // path for image storage
+  const imagePath = path.join(__dirname, './public/images/smobShops', `${i}.jpg`);
+  axios({
+    method: 'get',
+    url: url3[i],
     responseType: 'stream',
   })
     .then((response) => {
