5467bd98010df8e532ff41b7faabc24987bee1fe
fwornle <fwornle@yahoo.co.uk>
2022-01-22 20:50:34 +0100

    taken out auth to allow udacity to debug

--- code/app/src/main/java/com/tanfra/shopmob/smob/data/net/utils/AuthInterceptor.kt
+++ code/app/src/main/java/com/tanfra/shopmob/smob/data/net/utils/AuthInterceptor.kt
@@ -12,24 +12,24 @@ import okhttp3.Response
 class AuthInterceptor() : Interceptor {
 
     // fetch API key from build config parameter SMOD_NET_API_KEY, see: build.gradle (:app)
-    private val API_KEY = BuildConfig.SMOD_NET_API_KEY
+//    private val API_KEY = BuildConfig.SMOD_NET_API_KEY
 
     override fun intercept(chain: Interceptor.Chain): Response {
 
         var req = chain.request()
 
-        // need ("simple") auth?
-        if (!API_KEY.equals("null")) {
-
-            // yes, API_KEY given --> add as parameter (auth)
-            val url = req.url.newBuilder()
-                .addQueryParameter("APPID", API_KEY)
-                .build()
-
-            req = req.newBuilder()
-                .url(url).build()
-
-        }
+//        // need ("simple") auth?
+//        if (!API_KEY.equals("null")) {
+//
+//            // yes, API_KEY given --> add as parameter (auth)
+//            val url = req.url.newBuilder()
+//                .addQueryParameter("APPID", API_KEY)
+//                .build()
+//
+//            req = req.newBuilder()
+//                .url(url).build()
+//
+//        }
 
         // return (possibly) manipulated request
         return chain.proceed(req)
